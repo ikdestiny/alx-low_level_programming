d1f133897d565e9526fbbc0b0d17dfbd2b7a72b1
ikdestiny <uplifesuccess@gmail.com>
2021-06-19 17:50:18 -0700

    update

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -13,29 +13,31 @@
  */
 int main(void)
 {
-	int digit1, digit2, digit3;
+	int c = 0;
+	int f_d;
+	int m_d;
+	int l_d;
 
-	for (digit1 = 0; digit1 < 8; digit1++)
+	while (c <= 999)
 	{
-		for (digit2 = digit1 + 1; digit2 < 9; digit2++)
-		{
-			for (digit3 = digit2 + 1; digit3 < 10; digit3++)
-			{
-				putchar((digit1 % 10) + '0');
-				putchar((digit2 % 10) + '0');
-				putchar((digit3 % 10) + '0');
+		f_d = (c / 100 + '0');
+		m_d = (c / 10 % 10 + '0');
+		l_d = (c % 10 + '0');
 
-				if (digit1 == 7 && digit2 == 8 && digit3 == 9)
-					continue;
-				
+		if ((f_d < m_d) && (m_d < l_d))
+		{
+			putchar(f_d);
+			putchar(m_d);
+			putchar(l_d);
+
+			if (c != 789)
+			{
 				putchar(',');
 				putchar(' ');
 			}
 		}
+		c++;
 	}
-
 	putchar('\n');
-
 	return (0);
 }
-
