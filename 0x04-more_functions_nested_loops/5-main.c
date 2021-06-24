59e8c56f6de7785d5679e8236e230e370c563d12
ikdestiny <uplifesuccess@gmail.com>
2021-06-24 16:17:34 -0700

    update

---
+++ 0x04-more_functions_nested_loops/5-main.c
@@ -0,0 +1,25 @@
+#include "holberton.h"
+
+/**
+ * main - check the code for ALX School students.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+    more_numbers();
+    return (0);
+}
+julien@ubuntu:~/0x04$ gcc -Wall -pedantic -Werror -Wextra -std=gnu89 _putchar.c 5-main.c 5-more_numbers.c -o 5-more_numbers
+julien@ubuntu:~/0x04$ ./5-more_numbers 
+01234567891011121314
+01234567891011121314
+01234567891011121314
+01234567891011121314
+01234567891011121314
+01234567891011121314
+01234567891011121314
+01234567891011121314
+01234567891011121314
+01234567891011121314
+  
---
+++ 0x04-more_functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+More functions about c-program
